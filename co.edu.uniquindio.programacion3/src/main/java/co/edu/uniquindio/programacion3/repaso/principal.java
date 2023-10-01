a18767dc7900f37611148d5f3fed0258815e0be4
Jobany <jobanya.giraldos@uqvirtual.edu.co>
2023-09-30 20:58:53 -0500

    Agregue log

--- co.edu.uniquindio.programacion3/src/main/java/co/edu/uniquindio/programacion3/repaso/principal.java
+++ co.edu.uniquindio.programacion3/src/main/java/co/edu/uniquindio/programacion3/repaso/principal.java
@@ -2,7 +2,8 @@ package co.edu.uniquindio.programacion3.repaso;
 
 public class principal {
     public static void main(String[] args) {
-
+        var test ="test";
+        System.out.println(test);
     }
 
 }
